6fccd6f169fd41c660042c7f52f89fe106bb6e6e
PranjalAgni <pranjal.agnihotri6@gmail.com>
2020-12-13 17:51:03 +0530

    :green_heart: added database module

---
+++ src/database/database.provider.ts
@@ -0,0 +1,9 @@
+import { createConnection } from "typeorm";
+import ormconfig from "../../ormconfig";
+
+export const databaseProviders = [
+  {
+    provide: "DATABASE_CONNECTION",
+    useFactory: async () => await createConnection(ormconfig)
+  }
+];
